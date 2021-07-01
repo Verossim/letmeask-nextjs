d00ca58ae69c1d88b015764be299e3690642ad6c
Matheus Lima <matheuscaldas95@gmail.com>
2021-07-01 15:36:50 -0300

    Alteração nas configurações do tailwind
    Adição de pseudo classes no css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,30 +1,56 @@
 const colors = require('tailwindcss/colors')
+const plugin = require('tailwindcss/plugin')
 
 
 module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
-    colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
-      black: colors.black,
-      white: colors.white,
-      gray: colors.trueGray,
-      indigo: colors.indigo,
-      red: colors.rose,
-      yellow: colors.amber,
-      purple: {
-        DEFAULT: '#835afd'
+    extend: {
+      colors: {
+        purple: {
+          DEFAULT: '#835afd'
+        }
+      },
+      flex: {
+        '7': '7 7 0%',
+        '8': '8 8 0%'
+      },
+      fontFamily: {
+        'Poppins': ['Poppins, sans-serif'],
       }
-    },
-    flex: {
-      '7': '7'
     },
   },
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    plugin(({ addVariant, e }) => {
+      addVariant('before', ({ modifySelectors, separator }) => {
+        modifySelectors(({ className }) => {
+          return `.${e(`before${separator}${className}`)}::before`;
+        });
+      });
+      addVariant('after', ({ modifySelectors, separator }) => {
+        modifySelectors(({ className }) => {
+          return `.${e(`after${separator}${className}`)}::after`;
+        });
+      });
+    }),
+    plugin(({ addUtilities }) => {
+      const contentUtilities = {
+        '.content': {
+          content: 'attr(data-content)',
+        },
+        '.content-before': {
+          content: 'attr(data-before)',
+        },
+        '.content-after': {
+          content: 'attr(data-after)',
+        },
+      };
+
+      addUtilities(contentUtilities, ['before', 'after']);
+    })
+  ],
 }
