c2c80931df4a3dc1438185b6aa923729e90d0dbc
Matheus Lima <matheuscaldas95@gmail.com>
2021-07-02 11:30:28 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ const plugin = require('tailwindcss/plugin')
 
 
 module.exports = {
-  purge: ['./src/pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  purge: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
@@ -18,6 +18,10 @@ module.exports = {
       },
       fontFamily: {
         'Poppins': ['Poppins, sans-serif'],
+      },
+      fontSize: {
+        '2x1': '1.5rem',
+        '4x1': '2.25rem
       }
     },
   },
