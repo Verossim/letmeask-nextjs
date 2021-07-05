f68298c9e99254756f7e1abdf1a0df617dcfeadf
Matheus Lima <matheuscaldas95@gmail.com>
2021-07-05 14:22:19 -0300

    firebase env
    https://stackoverflow.com/questions/65264708/firebase-initializeapp-cant-find-the-api-key-in-my-dotenv-file-next-js

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -4,10 +4,10 @@ import 'firebase/auth'
 import 'firebase/database'
 
 const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    apiKey: "AIzaSyBviZuKOBk3M6fZzocyMcWgcF8FzMpAJsM",
+    authDomain: "letmeask-4d6f6.firebaseapp.com",
+    databaseURL: "https://letmeask-4d6f6-default-rtdb.firebaseio.com",
+    projectId: "letmeask-4d6f6",
     storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
     messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
     appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
